def90dbae12beecbab2ce62fa24d88d2aa76ae10
Namal Dayarathna <namal.dayarathna@redrow.co.uk>
2018-09-12 11:43:01 +0100

    Attempted to fix error when F was not returned

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,12 +20,12 @@ namespace GradeBook.GradeBooks
             // For every 20% of students that have a lower average, the grade rises by 1 position
             var studentsPerGradeIncrease = Students.Count / 5;
             var currentLetterGrade = 'F';
-            int studentIndex = 0;
-            int studentsWithLowerGrade = 0;
+            var studentIndex = 0;
+            var studentsWithLowerGrade = 0;
             while (currentLetterGrade > 'A' && studentIndex < Students.Count)
             {
                 // Keep track of how many students have a lower grade, which we use to calculate relative grade
-                if (Students[studentIndex].AverageGrade <= averageGrade)
+                if (Students[studentIndex].AverageGrade < averageGrade)
                 {
                     studentsWithLowerGrade++;
                     if (studentsWithLowerGrade >= studentsPerGradeIncrease)
