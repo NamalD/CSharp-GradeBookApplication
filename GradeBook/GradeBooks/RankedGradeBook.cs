44d83d1cd64b188d61e4b7cc8e520306697a229f
Namal Dayarathna <namal.dayarathna@redrow.co.uk>
2018-09-12 15:56:56 +0100

    Module 4 - override statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -44,5 +44,26 @@ namespace GradeBook.GradeBooks
             var gradeIncrease = (position * 5) / sortedGrades.Count; // Value is effectively floored
             return (char)(Convert.ToInt16('F') - gradeIncrease);
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
